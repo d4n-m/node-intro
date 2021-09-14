1e62866faa209401c02a1df85815f7f813da365a
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2021-09-15 01:35:13 +0200

    no news is good news

--- starter-code/index.js
+++ starter-code/index.js
@@ -3,11 +3,16 @@ class SortedList {
         this.items = [];
         this.length = this.items.length;
     }
+
     add(item) {  
         this.items.push(item);
-        // console.log(this.items.length)
+        console.log(this.items.length)
     }
-    get(pos) {}
+
+    get(pos) {
+        return this.items[pos];
+    }
+
     max() {}
     min() {}
     average() {}
